1b275a665e1a63b1e57f88b4e63b7108f3323c62
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-26 00:57:47 -0500

    set up project and working on Navbar

---
+++ my-portfolio-xx/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-blue text-slate-500'> Mayra Ibarra</div>
+  )
+}
+
+export default Navbar
