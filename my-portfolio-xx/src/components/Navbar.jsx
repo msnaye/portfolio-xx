c0ed993b249c1d8aeb69e0f1bbd096fa579eb1d0
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-26 01:25:21 -0500

    worked on Navbar

--- my-portfolio-xx/src/components/Navbar.jsx
+++ my-portfolio-xx/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
+    return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-blue text-slate-500'> Mayra Ibarra</div>
-  )
+    {/*Menu*/}
+    <div className='hidden'>
+       <ul className='flex'>
+           <li>Home</li>
+           <li>About</li>
+           <li>Services</li>
+           <li>Projects</li>
+           <li>Contact</li>
+       </ul>
+    </div>
+
+        {/*Hamburger*/}
+        <div className='hidden'>
+            <FaBars/>
+        </div>
+        {/*Menu for mobile device*/}
+        <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+        <div></div>
+        {/*Icons for social apps*/}
+    )
 }
 
 export default Navbar
