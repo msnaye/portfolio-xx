ff6386be0c9649e1a540decb238cdba9777d4b99
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-26 02:00:33 -0500

    worked on Navbar

--- my-portfolio-xx/src/components/Navbar.jsx
+++ my-portfolio-xx/src/components/Navbar.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
+    const [nav, setUpNav]= useState(false)
+    const handleClick =()=> setUpNav(!nav)
+
     return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-blue text-slate-500'> Mayra Ibarra</div>
-    {/*Menu*/}
-    <div className='hidden'>
-       <ul className='flex'>
-           <li>Home</li>
-           <li>About</li>
-           <li>Services</li>
-           <li>Projects</li>
-           <li>Contact</li>
-       </ul>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-700 text-slate-500'>
+    <div>
+
+    {/*Menu*/ }
+    <ul className='hidden md:flex'>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+    </ul>
+
+
+    {/*Hamburger*/ }
+    <div onClick={handleClick} className='md:hidden z-10'>
+      {!nav ?  <FaBars /> : <FaTimes/>}
     </div>
 
-        {/*Hamburger*/}
-        <div className='hidden'>
-            <FaBars/>
-        </div>
-        {/*Menu for mobile device*/}
-        <ul className='hidden'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul>
-        <div></div>
-        {/*Icons for social apps*/}
+
+    {/*Menu for mobile device*/ }
+    <ul className={!nav ? 'hidden' : absolute top-0 left-0 w-full justify-center items-center h-screen bg-slate-700}
+        <li className='py-7 text-3xl'>Home</li>
+        <li className='py-7 text-3xl'>About Me</li>
+        <li className='py-7 text-3xl'>Services</li>
+        <li className='py-7 text-3xl'>Projects</li>
+        <li className='py-7 text-3xl'>Contact</li>
+    </ul>
+
+    {/*Icons for social apps*/ }
+    <div className='hidden'></div>
+        </div >
     )
 }
 
